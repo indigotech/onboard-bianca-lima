d1f8cfbe7a39e3975e2c8c5a57c492bbfcf1f0f4
bialima2001 <bialimacel@gmail.com>
2024-08-08 11:12:31 -0300

    input validation

--- src/graphQL/resolvers.ts
+++ src/graphQL/resolvers.ts
@@ -9,6 +9,22 @@ const resolvers = {
   Mutation: {
     createUser: async (parent, args) => {
       const { data } = args;
+      if (data.password.length < 6) {
+        throw new Error('Password must be at least 6 characters long');
+      }
+
+      if (!/\d/.test(data.password) || !/[a-zA-Z]/.test(data.password)) {
+        throw new Error('Password must contain at least 1 letter and 1 digit');
+      }
+
+      const existingUser = await prisma.user.findUnique({
+        where: { email: data.email },
+      });
+
+      if (existingUser) {
+        throw new Error('Email is already in use');
+      }
+
       const newUser = await prisma.user.create({
         data: {
           name: data.name,

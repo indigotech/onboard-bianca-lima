e07e0e730508f84fc12cfe74bc29188b3232a4cb
bialima2001 <bialimacel@gmail.com>
2024-08-13 15:19:49 -0300

    update test/index.ts

--- test/index.ts
+++ test/index.ts
@@ -1,17 +1,19 @@
-import { describe, it, before } from 'mocha';
+import { describe, it, before, after } from 'mocha';
 import { expect } from 'chai';
 import axios from 'axios';
-
 import { startServer, stopServer } from '../src/server.js';
 
-describe('Apollo Server', () => {
-  let server;
-  before(async () => {
-    server = await startServer();
-  });
+let server;
+before(async () => {
+  server = await startServer();
+});
+after(async () => {
+  await stopServer(server);
+});
 
+describe('Apollo Server', () => {
   it('should return "Hello world!"', async () => {
-    const response = await axios.post('http://localhost:4000', {
+    const response = await axios.post('http://localhost:4001', {
       query: `
         query {
           hello
